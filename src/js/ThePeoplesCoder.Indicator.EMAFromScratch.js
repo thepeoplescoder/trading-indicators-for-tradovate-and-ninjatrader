53c51acbe9f9e5f7678991fda6a71da64013ccb0
dustinii <125335987+dustinii@users.noreply.github.com>
2024-01-05 13:34:44 -0800

    you scratch my back, ill scratch my back
    Taming the Wild Calculations - No more EMA-nating bugs, just smooth sailing ahead!

--- src/js/ThePeoplesCoder.Indicator.EMAFromScratch.js
+++ src/js/ThePeoplesCoder.Indicator.EMAFromScratch.js
@@ -10,33 +10,39 @@
 const predef = require("./tools/predef");
 
 class EMAFromScratch {
-    init() {
+    constructor() {
+        // Initialize variables for EMA calculation
         this.previousMA = undefined;
         this.initialSum = 0;
         this.MULTIPLIER = 2.0 / (this.props.period + 1);
     }
     
+    // Mapping function to calculate EMA for each data point
     map(d, index) {
-        this.previousMA = this.__weHaveEnoughValuesForAnEma(index)
-            ? this.__calculateEma(d)
-            : this.__updateInitialSumAndGetAverage(d, index);
+        this.previousMA = this.hasEnoughValuesForEMA(index)
+            ? this.calculateEMA(d)
+            : this.updateInitialSumAndGetAverage(d, index);
 
         return this.previousMA;
     }
 
+    // Filter function to determine if there are enough values for EMA calculation
     filter(_, index) {
-        return this.__weHaveEnoughValuesForAnEma(index);
+        return this.hasEnoughValuesForEMA(index);
     }
 
-    __weHaveEnoughValuesForAnEma(index) {
+    // Check if there are enough values to compute EMA
+    hasEnoughValuesForEMA(index) {
         return index >= this.props.period;
     }
 
-    __calculateEma(d) {
+    // Calculate EMA for the given data point
+    calculateEMA(d) {
         return this.previousMA + this.MULTIPLIER * (d.value() - this.previousMA);
     }
     
-    __updateInitialSumAndGetAverage(d, index) {
+    // Update initial sum and calculate the average for the first few values
+    updateInitialSumAndGetAverage(d, index) {
         this.initialSum += d.value();
         return this.initialSum / (index + 1);
     }
@@ -46,7 +52,7 @@ module.exports = {
     name: "ThePeoplesCoder.EMAFromScratch",
     calculator: EMAFromScratch,
     params: {
-        period: predef.paramSpecs.period(14)
+        period: predef.paramSpecs.period(14) // Default period for EMA calculation
     },
-    schemeStyles: predef.styles.solidLine("#95f57a")
+    schemeStyles: predef.styles.solidLine("#95f57a") // Scheme styles for plotting
 };
