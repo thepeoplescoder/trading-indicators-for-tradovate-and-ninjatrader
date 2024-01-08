718b0cebd46d1d88c12678f19321b87c42831370
John Doe <thepeoplescoder@gmail.com>
2024-01-07 21:07:07 -0800

    Figuring out how to load external CommonJS modules from within Tradovate.  Either the modern way doesn't work on Tradovate, or I am doing something wrong.

--- src/js/ThePeoplesCoder.Indicator.VWAP.js
+++ src/js/ThePeoplesCoder.Indicator.VWAP.js
@@ -10,7 +10,56 @@ const tools = Object.freeze({
     predef: require("./tools/predef"),
 });
 
-const WeightedAverage = require("./ThePeoplesCoder.Tool.WeightedAverage.js");
+const WeightedAverage = loadCommonJsModuleFromUrl([
+    "https://raw.githubusercontent.com/thepeoplescoder/trading-indicators-for-tradovate-and-ninjatrader",
+    "/main",
+    "/src/js/ThePeoplesCoder.Tool.WeightedAverage.js",
+].join(''));
+
+
+function loadCommonJsModuleFromUrl(url) {
+    return loadCommonJsModuleFromSourceCode(loadSourceCodeFromUrl(url));
+}
+
+function loadSourceCodeFromUrl(url) {
+    console.log("Syncrhonously loading source code from URL: " + url);
+    return theOldWay();
+
+    function theOldWay() {
+        console.log("Doing it the old way.");
+
+        const request = new XMLHttpRequest();
+        const result  = {};
+        
+        request.addEventListener("load",
+            () => (result.sourceCode = request.responseText));
+            
+        request.open("GET", url, false);
+        request.send();
+        
+        return result.sourceCode;
+    }
+
+    function theNewWay() {
+        const result = {};
+        (async () => {
+            console.log("Doing it the new way.");
+            const response    = await fetch(url);
+            result.sourceCode = await response.text();
+        })();
+        console.log("WHY DOES THIS NOT WORK??????");
+        console.log(JSON.stringify(result));
+        return result.sourceCode;
+    }
+}
+
+function loadCommonJsModuleFromSourceCode(src) {
+    console.log("Source code loaded:\n" + src);
+
+    const module = {exports: {}};
+    (new Function("module", "exports", src))(module, module.exports);
+    return module.exports;
+}
 
 class VWAP {
     init() {
@@ -28,9 +77,7 @@ class VWAP {
 module.exports = {
     name: "ThePeoplesCoder.VWAP",
     calculator: VWAP,
-
     description: "Volume Weighted Average Price",
     tags: ["com.thepeoplescoder",],
-
     schemeStyles: tools.predef.styles.solidLine("#95f57a")
 };
